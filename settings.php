db2adf8ba08b41a27696379cd2131ad9bd34bebe
Bas Brands <bas@sonsbeekmedia.nl>
2014-05-12 12:44:43 +0200

    update version

---
+++ settings.php
@@ -0,0 +1,55 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for slate details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Theme More settings.
+ *
+ * Each setting that is defined in the parent theme Clean should be
+ * defined here too, and use the exact same config name. The reason
+ * is that theme_slate does not define any layout files to re-use the
+ * ones from theme_clean. But as those layout files use the function
+ * {@link theme_clean_get_html_for_settings} that belong to Clean,
+ * we have to make sure it works as expected by having the same settings
+ * in our theme.
+ *
+ * @see        theme_clean_get_html_for_settings
+ * @package    theme_slate
+ * @copyright  2014 Frédéric Massart
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+if ($ADMIN->fulltree) {
+
+    // Logo file setting.
+    $name = 'theme_slate/logo';
+    $title = get_string('logo','theme_slate');
+    $description = get_string('logodesc', 'theme_slate');
+    $setting = new admin_setting_configstoredfile($name, $title, $description, 'logo');
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+
+    // Custom CSS file.
+    $name = 'theme_slate/customcss';
+    $title = get_string('customcss', 'theme_slate');
+    $description = get_string('customcssdesc', 'theme_slate');
+    $default = '';
+    $setting = new admin_setting_configtextarea($name, $title, $description, $default);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+
+}
